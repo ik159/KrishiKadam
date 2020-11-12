d78ee56888058678a183d27d5639b9c474fa6510
Ishan Kumar <itsik159@gmail.com>
2020-11-12 18:53:38 +0530

    updated section2

--- lib/components/section2/combine.dart
+++ lib/components/section2/combine.dart
@@ -1,8 +1,7 @@
 
 import 'package:flutter/material.dart';
-import 'package:krishikadam/components/Section2/crop.dart';
-import 'package:krishikadam/components/Section2/soil.dart';
-
+import 'package:krishikadam/components/section2/crop.dart';
+import 'package:krishikadam/components/section2/soil.dart';
 import '../bottom_nav.dart';
 
 class sect2 extends StatelessWidget {
@@ -19,93 +18,104 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
+
+double width , height;
+
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
     return Scaffold(
-        backgroundColor: Color(0xFFB9DA8F),
-          body: Column(
+        backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+                      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top:50.0, left: 30.0),
-            child: Container(
-              width: 380,
-                                height: 45,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                    color: Colors.black.withOpacity(.1),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20))),
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    hintText: "Search...",
-                                    border: InputBorder.none,
-                                    prefixIcon: Icon(Icons.search),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:25.0, horizontal: 30.0),
+              child: Container(
+        
+                                  height: height * .07,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(.1),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20))),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                      hintText: "Search...",
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(Icons.search),
+                                    ),
                                   ),
-                                ),
+              ),
             ),
-          ),
-          GestureDetector(
-            onTap: (){
-          Navigator.push(  context,
+            GestureDetector(
+              onTap: (){
+            Navigator.push(  context,
     MaterialPageRoute(builder: (context) => Soil()),
   );
         },
-                      child: Padding(
-              padding: const EdgeInsets.only(top:30.0, left: 20.0),
-              child: Container(
-                height: 200,
-                width: 380,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  image: DecorationImage(
-                          image: AssetImage('assets/images/soil.jpg'),
-                          fit: BoxFit.cover,
-                        ),
-                ),
-                child:  Padding(
-                  padding: const EdgeInsets.only(top:80.0, left: 80),
-                  child: Text('Soil',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40.0)),
+                        child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:15.0, horizontal: 20.0),
+                child: Container(
+                  height: height * .25,
+                  width: 380,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    image: DecorationImage(
+                            image: AssetImage('assets/images/soil.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                  child:  Padding(
+                    padding: const EdgeInsets.only(top:80.0, left: 80),
+                    child: Text('Soil',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40.0)),
+                  ),
                 ),
               ),
             ),
-          ),
-            GestureDetector(
-              onTap: (){
-          Navigator.push(  context,
+              GestureDetector(
+                onTap: (){
+            Navigator.push(  context,
     MaterialPageRoute(builder: (context) => Crop()),
   );
         },
-                          child: Padding(
-              padding: const EdgeInsets.only(top:20.0, left: 20.0),
-              child: Container(
-                height: 200,
-                width: 380,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.green,
-                  image: DecorationImage(
-                          image: AssetImage('assets/images/crops.jpg'),
-                          fit: BoxFit.cover,
-                        ),
+                            child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:15.0, horizontal: 20.0),
+                child: Container(
+                  height: height *0.25,
+                  width: 380,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green,
+                    image: DecorationImage(
+                            image: AssetImage('assets/images/crops.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                child:  Padding(
+                  padding: const EdgeInsets.only(top:80.0, left: 80.0),
+                  child: Text('Crops',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40.0)),
                 ),
-              child:  Padding(
-                padding: const EdgeInsets.only(top:80.0, left: 80.0),
-                child: Text('Crops',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40.0)),
-              ),
-              ),
-          ),
+                ),
             ),
-            BottomNav(),
+              ),
+            
         ],
+      ),
+          ),
+          bottomNavigationBar: BottomNav(
+        height: height,
+        width: width,
       ),
     );
   }
